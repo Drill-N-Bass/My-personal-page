a558e2acf235a9141d26140d69ad82ce188f20ba
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-09-14 21:21:45 +0200

    chomik test

--- django_my_page_pawel_pedryc/hangman_game/views.py
+++ django_my_page_pawel_pedryc/hangman_game/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
 #chomik
+=======
+# chomik
+>>>>>>> f3a11e7 (chomik test)
 def easter_egg_hangman_game(request, *args, **kwargs):
 	print("args, kwargs: ", args, kwargs) # checking args and kwargs in console
 	print("User's name that is log in now is: ", request.user) # checking username that is log in (in console)
