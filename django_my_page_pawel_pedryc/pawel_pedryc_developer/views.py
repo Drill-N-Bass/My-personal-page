b69b5189316d1a48cf88f26082b32f4ed0ac90ce
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-10-18 15:58:38 +0200

    update documentation

--- django_my_page_pawel_pedryc/pawel_pedryc_developer/views.py
+++ django_my_page_pawel_pedryc/pawel_pedryc_developer/views.py
@@ -47,10 +47,29 @@ def my_essays(request, home_view_pawel_slug):
             if user_feedback.is_valid():
                 user_email = user_feedback.cleaned_data['email']
                 send_me_message, was_created = SendMeMessage.objects.get_or_create(email=user_email) # 3.44.00
+                ## v1:
                 # send_me_message = user_feedback.save() # v1 problem: user can sign just once 3.41.00
                 # v2:
                 # user_email = user_feedback.cleaned_data['email'] # `email` key from SendMeMessage model 3.42.21
-                # send_me_message, _ = end_me_message.objects.get_or_create(email=user_email)
+                """
+                Having `user_email` object I can create a `send_me_message` by myself
+                I will do so, because if I use `send_me_message` model from models.py instead of this form
+                then I won't have control over how it's created. Doing the line of code below,
+                I have control over how it's created and I can check
+                whether I have added before an email before I try to create a new one in db. 
+                If I don't have an email in db I can create a new instance in db with the line:
+                `send_me_message_new, _ = send_me_message.objects.get_or_create(email=user_email)`.
+                I can write, also 
+                send_me_message_new, was_created = send_me_message.objects.get_or_create(email=user_email)
+                the flag `was_created` tells us whether a new entry was created or not.
+                `_` ignore value: `was_created`.
+
+            
+                """
+                # send_me_message_new, _ = send_me_message.objects.get_or_create(email=user_email)
+                # selected_essay.guest.add(send_me_message_new)
+                ## end v2
+
                 selected_essay.guest.add(send_me_message) # 3.26.00
                 return redirect('confirm-registration', home_view_pawel_slug=home_view_pawel_slug) # 3.52.00
                 
