ae72b47a8bb1a1c4a2fc1923cb478ce0691e1dc0
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-11-08 02:26:14 +0100

    fix issues with js displaying

---
+++ django_my_page_pawel_pedryc/static/pawel_pedryc/js/darkmode.js
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=wodWDIdV9BY
+
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+// test:
+darkModeToggle.addEventListener("click", () => {
+    console.log("The darkModeToggle test success !!!");
+});
