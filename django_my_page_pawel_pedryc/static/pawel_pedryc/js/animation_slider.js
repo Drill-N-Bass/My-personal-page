42ff7b728d75d1e9c8367c2f1fc4138bb6688569
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-11-16 19:22:18 +0100

    minor update of files

--- django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation_slider.js
+++ django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation_slider.js
@@ -2,80 +2,47 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
 function* indexGenerator() {
     let index = 1;
-    while (index < 78) {
+    while (index < 77) {
         yield index++;
     }
-    if (index = 78)
+    if (index = 77)
         yield* indexGenerator(); // https://stackoverflow.com/a/72149706/15372196
   };
-  
-// const number = indexGenerator();
-// console.log("gen output:");
-// console.log(gen.next().value);
-// console.log("gen output:");
-// console.log(gen.next().value);
-// console.log("gen output:");
-// console.log(gen.next().value);
-// console.log("gen output:");
-// console.log(gen.next().value);
-
 
 var number = indexGenerator();
 
 
 setInterval(function animationslider()
 {
-    
-    
-    // var testx = number.next().value;
-    // console.log("gen output:");
-    // console.log(testx);
-
-    
-    // var testx = number.next().value;
-    // console.log("gen output:");
-    // console.log(testx);
-
-
-
-    // testx++; 
-    // // if (testx>77) testx=1;
-    // console.log("gen output test:");
-    // console.log(testx);
-    // console.log("gen output test:");
-    // console.log(testx);
-
     var file = "<img src=\"../../../static/pawel_pedryc/logos/media/slides_banner/light_mushroom_python_" + number.next().value + ".jpg\" />";
     document.getElementById("animation-slider").innerHTML = file;
-    // setTimeout("animationslider()", 5000);
-
-
-    // number.next().value;
-    // console.log("gen output:");
-    // console.log(number);
-
-    // number++; 
-    // if (number>77) number=1;
-    // console.log("gen output:");
-    // console.log(number);
-
-    // for (let index = 0; index < number.length; index++) {
-    //     var num = number[index];
-    //     console.log(num)
-    // }
-    // var num = 1
-    // if looperpic()
-
-
-
- 
     
-    // var file = "<img src=\"../../../static/pawel_pedryc/logos/media/python_mushroom.jpeg\" />";
-    
-
-
-
     $("#animation-slider").fadeIn(1000);
     
     
 }, 100);
+
+
+
+
+// function* indexGenerator() {
+//     let index = 1;
+//     while (index < 28) {
+//         yield index++;
+//     }
+//     if (index = 28)
+//         yield* indexGenerator();
+//   };
+
+// var number = indexGenerator();
+
+
+// setInterval(function animationslider()
+// {
+//     var file = "<img src=\"../../../static/pawel_pedryc/logos/media/slides_banner_f/slide_" + number.next().value + ".jpg\" />";
+//     document.getElementById("animation-slider").innerHTML = file;
+    
+//     $("#animation-slider").fadeIn(10);
+    
+    
+// }, 100);
