e16a2d99865c1b73087ca2a3861cea24b21fe1a7
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-11-16 05:51:35 +0100

    banner animation working - beta version

--- django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation.js
+++ django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation.js
@@ -1,23 +1,13 @@
-const utils = require('./usun')
-
-utils.greeting()
-utils.test_cyfr(10, 20)
-
-const test = http.
-
-
-
-
-// const frameHeight = 102;
-// const frames = 15;
-// const div = document.getElementById("animation");
-// let frame = 0;
-// setInterval(function()
-//     {
-//         const frameOffset = (++frame % frames) * -frameHeight;
-//         div.style.backgroundPosition = "0px " + frameOffset + "px";
-//     }, 100
-// );
+const frameHeight = 102;
+const frames = 15;
+const div = document.getElementById("animation");
+let frame = 0;
+setInterval(function()
+    {
+        const frameOffset = (++frame % frames) * -frameHeight;
+        div.style.backgroundPosition = "0px " + frameOffset + "px";
+    }, 100
+);
 
 
 // console.log("range 10:");
