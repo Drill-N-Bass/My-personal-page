44cbe214186678a31d16fc814cbb994925e41cce
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-11-18 02:27:03 +0100

    first -almost - stable version of my animated logo

--- django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation_slider.js
+++ django_my_page_pawel_pedryc/static/pawel_pedryc/js/animation_slider.js
@@ -1,48 +1,39 @@
+// // The version built below flicker and because of that I decided to make a new approach (above)
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
-function* indexGenerator() {
-    let index = 1;
-    while (index < 77) {
-        yield index++;
-    }
-    if (index = 77)
-        yield* indexGenerator(); // https://stackoverflow.com/a/72149706/15372196
-  };
-
-var number = indexGenerator();
-
-
-setInterval(function animationslider()
-{
-    var file = "<img src=\"../../../static/pawel_pedryc/logos/media/slides_banner/light_mushroom_python_" + number.next().value + ".jpg\" />";
-    document.getElementById("animation-slider").innerHTML = file;
-    
-    $("#animation-slider").fadeIn(1000);
-    
-    
-}, 100);
-
-
-
-
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
 // function* indexGenerator() {
-//     let index = 1;
-//     while (index < 28) {
+//     let index = 0;
+//     while (index < 77) {
 //         yield index++;
 //     }
-//     if (index = 28)
-//         yield* indexGenerator();
+//     if (index == 77)
+//         yield* indexGenerator(); // https://stackoverflow.com/a/72149706/15372196
 //   };
 
 // var number = indexGenerator();
-
+// $("#animation-slider").fadeOut(50);
 
 // setInterval(function animationslider()
-// {
-//     var file = "<img src=\"../../../static/pawel_pedryc/logos/media/slides_banner_f/slide_" + number.next().value + ".jpg\" />";
+// {   
+//     var file = "<img src=\"../../../static/pawel_pedryc/logos/media/slides_banner/light_mushroom_python_" + number.next().value + ".jpg\" />";
 //     document.getElementById("animation-slider").innerHTML = file;
     
-//     $("#animation-slider").fadeIn(10);
+//     $("#animation-slider").fadeIn(50);
     
     
 // }, 100);
+
+
+// the version suggestet by gilly3 on the stack overflow also flicker  
+// https://stackoverflow.com/a/74467782/15372196
+
+const animationContainer = document.getElementById("animation-slider");
+for (let i = 0; i < 77; i++) {
+    const img = document.createElement("img");
+    img.src = `../../../static/pawel_pedryc/logos/media/slides_banner/light_mushroom_python_${i}.jpg`;
+    animationContainer.appendChild(img);
+}
+
+setInterval(() => animationContainer.appendChild(animationContainer.children[0]), 100);
+
+
