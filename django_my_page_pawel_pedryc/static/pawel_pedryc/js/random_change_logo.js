c1fa72bf98f7697f0373d7f9c06e461b998e4374
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2023-02-12 16:54:54 +0100

    Fixed js for random tittle font

--- django_my_page_pawel_pedryc/static/pawel_pedryc/js/random_change_logo.js
+++ django_my_page_pawel_pedryc/static/pawel_pedryc/js/random_change_logo.js
@@ -1,7 +1,13 @@
+  // Inspired by https://stackoverflow.com/questions/5897573/random-predefined-background-color-and-text-color-on-2-divs
+  
   $(document).ready(function(){
-    var fonts = ["'Black And White Picture', sans-serif;", "'Swanky and Moo Moo', cursive;", "'Monsieur La Doulaise', cursive;", "'Roboto Slab', sans-serif;"];  
+    var fonts = [
+      "'Black And White Picture', sans-serif",
+      "'Swanky and Moo Moo', cursive",
+      "'Swanky and Moo Moo', cursive",
+      "'Monsieur La Doulaise', cursive",
+      "'Roboto Slab', sans-serif"
+      ];  
     var rand = Math.floor(Math.random()*fonts.length); 
     $('#main-logo').css("font-family", fonts[rand]);
   });
-
-
